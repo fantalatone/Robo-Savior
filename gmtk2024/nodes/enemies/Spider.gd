32e40fec13aaf17e7482d426a4cd01bf437120da
FantaLaTone <fantalatone@gmail.com>
2024-08-18 22:38:25 +0300

    spider done

--- gmtk2024/nodes/enemies/Spider.gd
+++ gmtk2024/nodes/enemies/Spider.gd
@@ -4,48 +4,49 @@ var robot
 
 @onready var ANIM: AnimationPlayer = $Spider/AnimationPlayer
 @onready var AGENT: NavigationAgent3D = $Agent
+
 @onready var ATTACK_POINT : Marker3D = $"Attack Point"
 @onready var ATTACK_SPHERE := preload("res://nodes/player/bullet/melee_attack.tscn")
 
-var should_attack_robot : bool = false
-var attack_timer : Timer = Timer.new()
-var attack_cooldown : Timer = Timer.new()
+var should_attack : bool = false
+var prev_should_attack : bool = true
 
 const SPEED = 5.0
 
 func _ready() -> void:
-	attack_timer.one_shot = true
-	attack_timer.timeout.connect(attack)
-	add_child(attack_timer)
-	
-	attack_cooldown.one_shot = true
-	add_child(attack_cooldown)
-	
 	robot = Robot.instance
-	ANIM.play("Armature|IDLE")
+	ANIM.play("IDLE")
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * 2.0 * delta
 	
-	should_attack_robot = global_position.distance_to(robot.global_position) <= 3
 	AGENT.target_position = robot.global_position
+	should_attack = global_position.distance_to(robot.global_position) <= 3.5
+	if prev_should_attack != should_attack:
+		prev_should_attack = should_attack
+		if should_attack:
+			ANIM.play("ATTACK")
 	
-	if not should_attack_robot and is_on_floor():
+	if not should_attack and is_on_floor():
 		var next = AGENT.get_next_path_position()
 		var dir = (next - global_position).normalized()
 		
 		velocity = dir * SPEED
+		
+		ANIM.play("WALK")
+
+	if should_attack:
+		
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
 	
-	var dir = global_position.direction_to(robot.global_position)
-	var angle = Vector2(dir.x, dir.z).angle()
-	
-	rotation.y = move_toward(rotation.y, angle, delta * 100.0)
+	look_at(robot.global_position)
+	rotation.x = 0.0
 	
 	move_and_slide()
 
 func attack() -> void:
-	pass
-
-func _on_animation_finished(anim_name: StringName) -> void:
-	pass
+	var b : Node3D = ATTACK_SPHERE.instantiate()
+	get_tree().current_scene.add_child(b)
+	b.global_position = ATTACK_POINT.global_position
