c9daffbb9e8418f0f3f84494775c275edeb4c994
FantaLaTone <fantalatone@gmail.com>
2024-08-20 18:23:33 +0300

    ghost item shader

--- gmtk2024/nodes/items/ItemInteraction.gd
+++ gmtk2024/nodes/items/ItemInteraction.gd
@@ -4,14 +4,18 @@ class_name ItemInteraction
 @export var item_type : ITEM_TYPE
 var item_taken : bool = false
 
+@onready var GHOST = preload("res://nodes/maps/ghost-item.tres")
+
 func _take_item() -> bool:
 	if item_taken: return false
-	get_child(1).hide()
+	var m : MeshInstance3D = get_child(1).get_child(0)
+	m.material_override = GHOST
 	item_taken = true
 	return true
 
 func _drop_item() -> bool:
 	if not item_taken: return false
-	get_child(1).show()
+	var m : MeshInstance3D = get_child(1).get_child(0)
+	m.material_override = null
 	item_taken = false
 	return true
